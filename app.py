ad16ba6e0f8005b9b56599ca38b9aaa6a38e5889
Sebaastian00 <114770041+Sebaastian00@users.noreply.github.com>
2025-05-24 09:49:45 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,26 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, redirect, url_for
+from azure.storage.blob import BlobServiceClient
 
 app = Flask(__name__)
 
-@app.route('/sensor-data', methods=['POST'])
-def sensor_data():
-    data = request.json
-    print("Otrzymane dane:", data)
-    return jsonify({"status": "OK"}), 200
+# Dane do Azure Blob Storage — wpisz swoje
+connect_str = "TWOJ_CONNECTION_STRING"
+container_name = "mycontainer"
+
+blob_service_client = BlobServiceClient.from_connection_string(connect_str)
+
+@app.route('/upload', methods=['POST'])
+def upload_file():
+    if 'file' not in request.files:
+        return "No file part"
+    file = request.files['file']
+    if file.filename == '':
+        return "No selected file"
+    
+    blob_client = blob_service_client.get_blob_client(container=container_name, blob=file.filename)
+    blob_client.upload_blob(file, overwrite=True)  # przesyła plik do Azure
+
+    return "File uploaded to Azure Blob Storage successfully!"
 
 if __name__ == '__main__':
     app.run(debug=True)
